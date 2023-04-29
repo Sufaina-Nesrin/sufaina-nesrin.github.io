250ed1e72614d0e1ae5bc5d6141c6e47b339397e
Sufaina-Nesrin <sufainanasrin2003@gmail.com>
2023-04-29 16:40:06 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$(window).on('load',function(){
+    $('.loader-wrapper').fadeOut("slow");
+ })
+      var typed = new Typed('#element', {
+        strings: ['Web Designer', 'Web Developer','Digital Marketer'],
+        typeSpeed: 100,
+        loop:true
+      });
+      
+     
