bf5009fa00bf05bb993abb032a0f41cbaf58fedc
Sufaina-Nesrin <sufainanasrin2003@gmail.com>
2024-01-14 14:40:24 +0530

    some-changes-made

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(window).on('load',function(){
     $('.loader-wrapper').fadeOut("slow");
  })
       var typed = new Typed('#element', {
-        strings: ['Web Designer', 'Web Developer','Digital Marketer'],
+        strings: ['Web Designer', 'Web Developer'],
         typeSpeed: 100,
         loop:true
       });
